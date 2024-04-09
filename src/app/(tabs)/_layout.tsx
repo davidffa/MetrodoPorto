a844b22bf4cdc0d6580577e5b596c6b708d264af
Gabriel Santos <gvds@ua.pt>
2024-04-09 14:54:43 +0100

    Tabs added (with icons)

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
 import { Tabs } from 'expo-router';
 import { Header } from '../components/header';
-import { Feather } from '@expo/vector-icons';
+import { Feather, Fontisto, Ionicons } from '@expo/vector-icons';
+import { Menu } from '../components/menu';
 
 export default function TabLayout() {
   return (
@@ -9,6 +10,31 @@ export default function TabLayout() {
         name="index"
         options={{
           tabBarIcon: ({ color }) => <Feather name="map" size={28} color={color} />,
+          header: () => <Header />,
+          tabBarLabel: () => null
+        }}
+      />
+      <Tabs.Screen
+        name="tickets"
+        options={{
+          tabBarIcon: ({ color }) => <Ionicons name="ticket-outline" size={28} color={color} />,
+          header: () => <Header />,
+          tabBarLabel: () => null
+        }}
+      />
+      <Tabs.Screen
+        name="train"
+        options={{
+          tabBarIcon: ({ color }) => <Fontisto name="train" size={28} color={color} />,
+          header: () => <Header />,
+          tabBarLabel: () => null
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        // component={Menu}
+        options={{
+          tabBarIcon: ({ color }) => <Feather name="menu" size={28} color={color} />,
           header: () => <Header />,
           tabBarLabel: () => null
         }}
---
+++ src/app/components/menu.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export function Menu() {
+    return(
+        <View>
+            <Text>
+                Hey
+            </Text>
+        </View>
+    )
+}
