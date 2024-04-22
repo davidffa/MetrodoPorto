02afa0b85e225efec2685eff68b867978cca5f11
David Amorim <davidfilipeamorim@gmail.com>
2024-04-22 11:25:04 +0100

    Fix pass buttons spacing

--- src/app/components/passe.tsx
+++ src/app/components/passe.tsx
@@ -1,55 +1,53 @@
-import { Text, ViewBase, View, TouchableOpacity } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
 import { Ionicons } from '@expo/vector-icons';
 
 type PassProps = {
-    type: string;
-    expireDate?: string;
-    usePass: (id: string) => void;
-    managePass: (id: string) => void;
+  type: string;
+  expireDate?: string;
+  usePass: (id: string) => void;
+  managePass: (id: string) => void;
 }
 
-
 export function Passe({ type, expireDate, usePass, managePass }: PassProps) {
-    return (
-        <View className="px-5 py-8 border border-blue-800 rounded-md bg-white w-full mt-4 flex-row justify-between items-center" >
-            <View className="gap-3">
-                <Text className="font-semibold text-l text-slate-800">
-                    {type}
-                </Text>
-                {
-
-                    expireDate && (
-                        <View className="flex-row items-center gap-1">
-                            <Ionicons name="warning-outline" size={20} color="orange" />
-
-
-                            <Text className="font-regular text-xs text-slate-800">
+  return (
+    <View className="px-5 py-8 border border-blue-800 rounded-md bg-white w-full mt-4 flex-row justify-between items-center" >
+      <View className="gap-3">
+        <Text className="font-semibold text-l text-slate-800">
+          {type}
+        </Text>
+        {
+          expireDate && (
+            <View className="flex-row items-center gap-1">
+              <Ionicons name="warning-outline" size={20} color="orange" />
 
 
+              <Text className="font-regular text-xs text-slate-800">
 
-                                {expireDate}
-                            </Text>
 
-                        </View>
-                    )
-                }
+
+                {expireDate}
+              </Text>
+
             </View>
+          )
+        }
+      </View>
 
-            <View className="flex-row px-40 gap-1 absolute">
+      <View className="flex-row gap-1">
 
-                <TouchableOpacity className="bg-green-800 items-center justify-center px-6 py-3 rounded-md" onPress={() => managePass("abc")}>
+        <TouchableOpacity className="bg-green-800 items-center justify-center px-6 py-3 rounded-md" onPress={() => managePass("abc")}>
 
-                    <Text className="font-bold text-white text-l">
-                        Gerir
-                    </Text>
-                </TouchableOpacity>
-                <TouchableOpacity className="bg-blue-800 items-center justify-center px-6 py-3 rounded-md" onPress={() => usePass("abc")}>
+          <Text className="font-bold text-white text-l">
+            Gerir
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity className="bg-blue-800 items-center justify-center px-6 py-3 rounded-md" onPress={() => usePass("abc")}>
 
-                    <Text className="font-bold text-white text-l">
-                        Usar
-                    </Text>
-                </TouchableOpacity>
-            </View>
-        </View>
-    )
+          <Text className="font-bold text-white text-l">
+            Usar
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
 }
--- src/app/components/ticket.tsx
+++ src/app/components/ticket.tsx
@@ -1,33 +1,34 @@
-import { Text, ViewBase, View, TouchableOpacity } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
 
 type TicketProps = {
-    type: string;
-    quantity: number;
-    id: string;
+  type: string;
+  quantity: number;
+  id: string;
 }
 
 
 export function Ticket({ type, quantity }: TicketProps) {
-    function useTicket() {
-        console.log("Ticket used")
-    }
-    return (
-        <View className="p-7 border border-blue-800 rounded-md bg-white w-full mt-4 flex-row justify-between items-center" >
-            <View className="gap-4">
-                <Text className="font-semibold text-l text-slate-800">
-                    {type}
-                </Text>
-                <Text className="font-semibold text-l text-slate-800">
-                    Quantidade: {quantity}
-                </Text>
-            </View>
+  function useTicket() {
+    console.log("Ticket used")
+  }
 
-            <TouchableOpacity className="bg-blue-800 items-center justify-center px-5 py-2 rounded-md" onPress={useTicket}>
+  return (
+    <View className="p-7 border border-blue-800 rounded-md bg-white w-full mt-4 flex-row justify-between items-center" >
+      <View className="gap-4">
+        <Text className="font-semibold text-l text-slate-800">
+          {type}
+        </Text>
+        <Text className="font-semibold text-l text-slate-800">
+          Quantidade: {quantity}
+        </Text>
+      </View>
 
-                <Text className="font-bold text-white text-xl">
-                    Usar
-                </Text>
-            </TouchableOpacity>
-        </View>
-    )
+      <TouchableOpacity className="bg-blue-800 items-center justify-center px-5 py-2 rounded-md" onPress={useTicket}>
+
+        <Text className="font-bold text-white text-xl">
+          Usar
+        </Text>
+      </TouchableOpacity>
+    </View>
+  )
 }
