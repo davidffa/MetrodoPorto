1e970cd78665d36db46907fda0e2826defee38a6
David Amorim <davidfilipeamorim@gmail.com>
2024-05-03 18:28:57 +0100

    Cleanup

--- src/app/components/drop-down.tsx
+++ src/app/components/drop-down.tsx
@@ -1,27 +1,44 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-export function Dropdown({ title, content }) {
-    const [isOpen, setIsOpen] = useState(false);
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { clsx } from "clsx";
 
-    const toggleDropdown = () => {
-        setIsOpen(!isOpen);
-    };
+type Props = {
+  title: string;
+  content: string;
+};
 
-    return (
-        
-            <View className="flex-col items-center justify-center mt-8 mx-6">
+export function Dropdown({ title, content }: Props) {
+  const [isOpen, setIsOpen] = useState(false);
 
-            <TouchableOpacity className="w-full flex-row items-center justify-center bg-white p-6 rounded-lg border border-gray-400" onPress={toggleDropdown}>
-                <Text className="text-lg font-blond">{title}</Text>
-                <Ionicons className="ml-auto"
-          name={isOpen ? 'chevron-up' : 'chevron-down'}
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <View className="flex-col items-center justify-center">
+      <TouchableOpacity
+        className={clsx("w-full flex-row items-center justify-center bg-white p-6 border border-gray-400", {
+          "rounded-t-lg": isOpen,
+          "rounded-lg": !isOpen
+        })}
+        activeOpacity={0.7}
+        onPress={toggleDropdown}
+      >
+        <Text className="text-2xl font-bold">{title}</Text>
+        <Ionicons className="ml-auto"
+          name={isOpen ? "chevron-up" : "chevron-down"}
           size={24}
           color="black"
         />
-            </TouchableOpacity>
-            {isOpen && <View className="w-full flex-row bg-white p-6 border border-gray-400 rouded-lg">{content}</View>}
-        </View>
-        
-    );
+      </TouchableOpacity>
+      {
+        isOpen && (
+          <View className="w-full flex-row bg-white p-6 border border-gray-400 rounded-b-lg">
+            <Text className="font-regular">{content}</Text>
+          </View>
+        )
+      }
+    </View >
+  );
 };
--- src/app/more/informacoes.tsx
+++ src/app/more/informacoes.tsx
@@ -1,52 +1,57 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Dropdown } from '../components/drop-down';
-import { useRouter } from 'expo-router';
-import { ScrollView } from 'react-native';
+import React from "react";
+import { Dropdown } from "../components/drop-down";
+import { ScrollView } from "react-native";
+
+// TODO: Move to utils
+const info = [
+  {
+    title: "Perdidos e Achados",
+    content: "Os objetos encontrados nos metros e estações devem ser entregues a um segurança da estação ou num dos Gabinetes de Apoio ao Cliente. Se perdeu algum objeto nas nossas instalações, deve dirigir- se a um Gabinete de Apoio ao Cliente, a um segurança, ou contactar telefonicamente a Linha de Apoio ao Cliente, para obter informação relativa ao eventual local onde se encontra o objeto perdido."
+  },
+  {
+    title: "Horários",
+    content: "Horários de funcionamento",
+  },
+  {
+    title: "Perdidos e Achados",
+    content: "Foste rickrolled"
+  },
+  {
+    title: "Perdidos e Achados",
+    content: "Foste rickrolled"
+  },
+  {
+    title: "Perdidos e Achados",
+    content: "Foste rickrolled"
+  },
+  {
+    title: "Perdidos e Achados",
+    content: "Foste rickrolled"
+  },
+  {
+    title: "Perdidos e Achados",
+    content: "Foste rickrolled"
+  },
+  {
+    title: "Perdidos e Achados",
+    content: "Foste rickrolled"
+  },
+]
 
 export default function Informacoes() {
-    const router = useRouter();
-    return (
-        <ScrollView>
-            <View className="flex-1 justify-center ">
-
-                <Dropdown
-                    title="Perdidos e Achados"
-                    content={<Text>Os objetos encontrados nos metros e estações devem ser entregues a um segurança da estação ou num dos Gabinetes de Apoio ao Cliente.
-
-                        Se perdeu algum objeto nas nossas instalações, deve dirigir-se a um Gabinete de Apoio ao Cliente, a um segurança, ou contactar telefonicamente a Linha de Apoio ao Cliente, para obter informação relativa ao eventual local onde se encontra o objeto perdido.</Text>}
-                />
-                <Dropdown
-                    title="Horários"
-                    content={<Text>Horários de funcionamento</Text>}
-                />
-                <Dropdown
-                    title="Perdidos e Achados"
-                    content={<Text>Foste rickrolled</Text>}
-                />
-                <Dropdown
-                    title="Horários"
-                    content={<Text>Horários de funcionamento</Text>}
-                />
-                <Dropdown
-                    title="Perdidos e Achados"
-                    content={<Text>Foste rickrolled</Text>}
-                />
-                <Dropdown
-                    title="Horários"
-                    content={<Text>Horários de funcionamento</Text>}
-                />
-                <Dropdown
-                    title="Perdidos e Achados"
-                    content={<Text>Foste rickrolled</Text>}
-                />
-                <Dropdown
-                    title="Horários"
-                    content={<Text>Horários de funcionamento</Text>}
-                />
-            </View>
-        </ScrollView>
-    );
+  return (
+    <ScrollView contentContainerStyle={{ padding: 24, gap: 28 }}>
+      {
+        info.map((item, idx) =>
+          <Dropdown
+            key={idx}
+            title={item.title}
+            content={item.content}
+          />
+        )
+      }
+    </ScrollView>
+  );
 };
 
 
