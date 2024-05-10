30e5cfd52d8a7023d5fccdec06ad94567900b0ad
Francisca Silva <franciscafpsilva@ua.pt>
2024-05-10 11:55:25 +0100

    Implement redirection to header
    Closes #7

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,10 +1,20 @@
-import { Image, Text, View } from "react-native";
+import { Image, Text, TouchableWithoutFeedback, View } from "react-native";
+import { useRouter } from "expo-router";
 
 export function Header() {
+
+  const router = useRouter();
+
+  function goHome() {
+    router.push("/buy-ticket/");
+  }
+
   return (
-    <View className="flex-row bg-blue-800 items-center justify-around px-10 py-6 pt-10">
-      <Image source={require("@/assets/logo.png")} />
-      <Text className="text-white font-bold text-3xl">Metro do Porto</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={goHome}>
+      <View className="flex-row bg-blue-800 items-center justify-around px-10 py-6 pt-10" >
+        <Image source={require("@/assets/logo.png")} />
+        <Text className="text-white font-bold text-3xl">Metro do Porto</Text>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
